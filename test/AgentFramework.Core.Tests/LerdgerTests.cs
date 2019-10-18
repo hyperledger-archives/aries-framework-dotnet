fbf565bfb424c994517c7fd7e3836b802ad3345a
Tomislav Markovski <tmarkovski@gmail.com>
2019-10-18 11:51:18 -0400

    Added LookupAmlAsync to Ledger Service
    Signed-off-by: Tomislav Markovski <tmarkovski@gmail.com>

--- test/AgentFramework.Core.Tests/LerdgerTests.cs
+++ test/AgentFramework.Core.Tests/LerdgerTests.cs
@@ -4,6 +4,8 @@ using AgentFramework.TestHarness;
 using Xunit;
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics;
+using Hyperledger.Indy.LedgerApi;
+using System;
 
 namespace AgentFramework.Core.Tests
 {
@@ -11,11 +13,23 @@ namespace AgentFramework.Core.Tests
     {
         //protected override string GetPoolName() => "sovrin-builder";
         
-        [Fact(DisplayName = "Get TAA from ledger if exists")]
+        [Fact(DisplayName = "Get Transaction Author Agreement from ledger if exists")]
         public async Task GetTaaFromLedger()
         {
             var taa = await Host.Services.GetService<ILedgerService>()
                 .LookupTaaAsync(Context);
+
+            var aml = await Host.Services.GetService<ILedgerService>()
+                .LookupAmlAsync(Context);
+
+            Assert.True(true);
+        }
+
+        [Fact(DisplayName = "Get Acceptance Mechanisms List from ledger if exists")]
+        public async Task GetAmlFromLedger()
+        {
+            var aml = await Host.Services.GetService<ILedgerService>()
+                .LookupAmlAsync(Context);
 
             Assert.True(true);
         }
