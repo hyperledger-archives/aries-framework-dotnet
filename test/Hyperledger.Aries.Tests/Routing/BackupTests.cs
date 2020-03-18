864c71c6e99c4e3a0768db210cbeeafc4fac4342
Dule Pop Andov <dule@codechem.com>
2020-03-18 09:37:10 +0100

    clean-up backup tests
    Signed-off-by: Dule Pop Andov <dule@codechem.com>

--- test/Hyperledger.Aries.Tests/Routing/BackupTests.cs
+++ test/Hyperledger.Aries.Tests/Routing/BackupTests.cs
@@ -3,17 +3,13 @@ using Hyperledger.Aries.Routing;
 using Hyperledger.TestHarness.Mock;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Mail;
-using System.Text;
 using System.Threading.Tasks;
 using Hyperledger.Aries.Configuration;
-using Hyperledger.Aries.Extensions;
 using Hyperledger.Aries.Storage;
 using Hyperledger.Indy.DidApi;
-using Hyperledger.Indy.WalletApi;
 using Microsoft.Extensions.Options;
 using Xunit;
 
@@ -24,43 +20,27 @@ namespace Hyperledger.Aries.Tests.Routing
         public InProcAgent.PairedAgents Pair { get; private set; }
         
         public IEdgeClientService EdgeClient { get; private set; }
-        public IEdgeClientService RetrieveEdgeClient { get; private set; }
-        
         public IAgentContext EdgeContext { get; private set; }
-        public IAgentContext RetrieverEdgeContext { get; private set; }
-        
         public AgentOptions AgentOptions { get; private set; }
-        public AgentOptions RetrieverAgentOptions { get; private set; }
-        
         public IAgentContext MediatorContext { get; private set; }
-        
         public IWalletService WalletService { get; private set; }
 
         public async Task DisposeAsync()
         {
             await Pair.Agent1.DisposeAsync();
             await Pair.Agent2.DisposeAsync();
-            await Pair.Agent3.DisposeAsync();
         }
 
         public async Task InitializeAsync()
         {
             // Agent1 - Mediator
             // Agent2 - Edge
-            // Agent3 - RetrieveEdge
             Pair = await InProcAgent.CreatePairedWithRoutingAsync();
 
             WalletService = Pair.Agent2.Host.Services.GetRequiredService<IWalletService>();
-            
             EdgeClient = Pair.Agent2.Host.Services.GetRequiredService<IEdgeClientService>();
-//            RetrieveEdgeClient = Pair.Agent3.Host.Services.GetRequiredService<IEdgeClientService>();
-            
             AgentOptions = Pair.Agent2.Host.Services.GetRequiredService<IOptions<AgentOptions>>().Value;
-//            RetrieverAgentOptions = Pair.Agent3.Host.Services.GetRequiredService<IOptions<AgentOptions>>().Value;
-            
             EdgeContext = Pair.Agent2.Context;
-//            RetrieverEdgeContext = Pair.Agent3.Context;
-            
             MediatorContext = Pair.Agent1.Context;
         }
 
@@ -71,7 +51,7 @@ namespace Hyperledger.Aries.Tests.Routing
 
             var path = SetupDirectoriesAndReturnPath(seed);
             
-            var r = await EdgeClient.CreateBackupAsync(EdgeContext, seed);
+            await EdgeClient.CreateBackupAsync(EdgeContext, seed);
             var numDirsAfterBackup = Directory.GetDirectories(path).Length;
             var walletDir = Directory.GetDirectories(path).First();
             var backupDir = Directory.GetDirectories(walletDir).First();
@@ -114,9 +94,9 @@ namespace Hyperledger.Aries.Tests.Routing
             var seed = "00000000000000000000000000000000";
 
             SetupDirectoriesAndReturnPath(seed);
-            var backupId = await EdgeClient.CreateBackupAsync(EdgeContext, seed);
+            await EdgeClient.CreateBackupAsync(EdgeContext, seed);
             
-            var result = await RetrieveEdgeClient.RetrieveBackupAsync(RetrieverEdgeContext, seed);
+            var result = await EdgeClient.RetrieveBackupAsync(EdgeContext, seed);
             
             Assert.NotEmpty(result);
             Assert.IsType<Attachment>(result.First());
@@ -126,9 +106,8 @@ namespace Hyperledger.Aries.Tests.Routing
         public async Task RestoreAgentFromBackup()
         {
             var seed = "00000000000000000000000000000000";
-            var path = SetupDirectoriesAndReturnPath(seed);
             
-            var backupId = await EdgeClient.CreateBackupAsync(EdgeContext, seed);
+            await EdgeClient.CreateBackupAsync(EdgeContext, seed);
             // Create a DID that we will retrieve and compare from imported wallet
             var myDid = await Did.CreateAndStoreMyDidAsync(EdgeContext.Wallet, "{}");
             
