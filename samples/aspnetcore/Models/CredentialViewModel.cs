405c2dbf48c80f69efd65c58aeb21ddd25bd36a4
conanoc <conanoc@gmail.com>
2020-04-16 19:16:44 +0900

    Add credential issuance scenarios
    Signed-off-by: conanoc <conanoc@gmail.com>

--- samples/aspnetcore/Models/CredentialViewModel.cs
+++ samples/aspnetcore/Models/CredentialViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using Hyperledger.Aries.Features.IssueCredential;
 
 namespace WebAgent.Models
 {
     public class CredentialViewModel
     {
-        public string Name { get; set; }
+        public string SchemaId { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
         public CredentialState State { get; set; }
+
+        public IEnumerable<CredentialPreviewAttribute> CredentialAttributesValues { get; set; }
     }
 }
