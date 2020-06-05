ab0fb85fc33c0649004b33a56f85330b1641a1a2
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-05 19:00:02 +0000

    Add GetWeatherForecastRequestValidator and tests
    These are their to complete the default dotnet core example

---
+++ samples/blazorHosted/Source/Api/Features/WeatherForecast/GetList/GetWeatherForecastsRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorHosted.Features.WeatherForecasts
+{
+  using FluentValidation;
+
+  public class GetWeatherForecastsRequestValidator : AbstractValidator<GetWeatherForecastsRequest>
+  {
+
+    // Inject Dependencies via contstructor
+    public GetWeatherForecastsRequestValidator()
+    {
+      RuleFor(aGetWeatherForecastRequest => aGetWeatherForecastRequest.Days)
+        .GreaterThan(0);
+    }
+  }
+}
