655f4a08582a87502418cf519e18fd8427572e74
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-09 12:22:44 +0000

    Update to latest template

---
+++ samples/blazorHosted/Source/Api/Features/Infrastructure/InternalsVisibleToClientAndServer.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+
+[assembly: InternalsVisibleTo("BlazorHosted.Client")]
+[assembly: InternalsVisibleTo("BlazorHosted.Client.Integration.Tests")]
+[assembly: InternalsVisibleTo("BlazorHosted.Server")]
+[assembly: InternalsVisibleTo("BlazorHosted.Server.Integration.Tests")]
