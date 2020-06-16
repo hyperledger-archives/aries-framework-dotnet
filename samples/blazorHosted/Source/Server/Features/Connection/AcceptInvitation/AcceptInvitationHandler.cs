1e558e49cb36f53343a1bd22fbe017305fe51d98
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-16 12:19:57 +0000

    AcceptInvitation Implementation

--- samples/blazorHosted/Source/Server/Features/Connection/AcceptInvitation/AcceptInvitationHandler.cs
+++ samples/blazorHosted/Source/Server/Features/Connection/AcceptInvitation/AcceptInvitationHandler.cs
@@ -1,5 +1,8 @@
 namespace BlazorHosted.Features.Connections
 {
+  using Hyperledger.Aries.Agents;
+  using Hyperledger.Aries.Features.DidExchange;
+  using Hyperledger.Aries.Utils;
   using MediatR;
   using System;
   using System.Collections.Generic;
@@ -9,6 +12,21 @@ namespace BlazorHosted.Features.Connections
   
   public class AcceptInvitationHandler : IRequestHandler<AcceptInvitationRequest, AcceptInvitationResponse>
   {
+    private readonly IAgentProvider AgentProvider;
+    private readonly IConnectionService ConnectionService;
+    private readonly IMessageService MessageService;
+
+    public AcceptInvitationHandler
+    (
+      IAgentProvider aAgentProvider,
+      IConnectionService aConnectionService,
+      IMessageService aMessageService
+    )
+    {
+      AgentProvider = aAgentProvider;
+      ConnectionService = aConnectionService;
+      MessageService = aMessageService;
+    }
 
     public async Task<AcceptInvitationResponse> Handle
     (
@@ -16,9 +34,20 @@ namespace BlazorHosted.Features.Connections
       CancellationToken aCancellationToken
     )
     {
+      IAgentContext agentContext = await AgentProvider.GetContextAsync();
+
+      ConnectionInvitationMessage connectionInvitationMessage = 
+        MessageUtils
+          .DecodeMessageFromUrlFormat<ConnectionInvitationMessage>(aAcceptInvitationRequest.InvitationDetails);
+
+      (ConnectionRequestMessage connectionRequestMessage, ConnectionRecord connectionRecord) 
+        = await ConnectionService.CreateRequestAsync(agentContext, connectionInvitationMessage);
+
+      await MessageService.SendAsync(agentContext.Wallet, connectionRequestMessage, connectionRecord);
+
       var response = new AcceptInvitationResponse(aAcceptInvitationRequest.CorrelationId);
 
-      return await Task.Run(() => response);
+      return response;
     }
   }
 }
