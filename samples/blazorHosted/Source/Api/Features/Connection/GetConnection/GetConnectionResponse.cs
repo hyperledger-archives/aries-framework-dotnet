a8f356a95790e49ff18d70bfc9cf19ba7f012414
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-17 04:45:16 +0000

    Implement GetConnection

--- samples/blazorHosted/Source/Api/Features/Connection/GetConnection/GetConnectionResponse.cs
+++ samples/blazorHosted/Source/Api/Features/Connection/GetConnection/GetConnectionResponse.cs
@@ -3,11 +3,16 @@ namespace BlazorHosted.Features.Connections
   using System;
   using System.Collections.Generic;
   using BlazorHosted.Features.Bases;
+  using Hyperledger.Aries.Features.DidExchange;
 
   public class GetConnectionResponse : BaseResponse
   {
+    public ConnectionRecord ConnectionRecord { get; set; } = null!;
     public GetConnectionResponse() { }
 
-    public GetConnectionResponse(Guid aCorrelationId) : base(aCorrelationId) { }
+    public GetConnectionResponse(Guid aCorrelationId, ConnectionRecord aConnectionRecord) : base(aCorrelationId) 
+    {
+      ConnectionRecord = aConnectionRecord;
+    }
   }
 }
--- samples/blazorHosted/Source/Server/Features/Connection/GetConnection/GetConnectionHandler.cs
+++ samples/blazorHosted/Source/Server/Features/Connection/GetConnection/GetConnectionHandler.cs
@@ -1,5 +1,7 @@
 namespace BlazorHosted.Features.Connections
 {
+  using Hyperledger.Aries.Agents;
+  using Hyperledger.Aries.Features.DidExchange;
   using MediatR;
   using System;
   using System.Collections.Generic;
@@ -9,6 +11,14 @@ namespace BlazorHosted.Features.Connections
   
   public class GetConnectionHandler : IRequestHandler<GetConnectionRequest, GetConnectionResponse>
   {
+    private readonly IAgentProvider AgentProvider;
+    private readonly IConnectionService ConnectionService;
+
+    public GetConnectionHandler(IAgentProvider aAgentProvider, IConnectionService aConnectionService)
+    {
+      AgentProvider = aAgentProvider;
+      ConnectionService = aConnectionService;
+    }
 
     public async Task<GetConnectionResponse> Handle
     (
@@ -16,9 +26,12 @@ namespace BlazorHosted.Features.Connections
       CancellationToken aCancellationToken
     )
     {
-      var response = new GetConnectionResponse(aGetConnectionRequest.CorrelationId);
 
-      return await Task.Run(() => response);
+      IAgentContext agentContext = await AgentProvider.GetContextAsync();
+
+      ConnectionRecord connectionRecord = (await ConnectionService.ListAsync(agentContext))
+        .FirstOrDefault(aConnectionRecord => aConnectionRecord.Id == aGetConnectionRequest.ConnectionId);
+      return new GetConnectionResponse(aGetConnectionRequest.CorrelationId, connectionRecord);
     }
   }
 }
