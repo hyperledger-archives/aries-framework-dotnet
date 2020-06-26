a754a53281f442edf7c1ad3ab7fd6d37f9bc7fa6
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-26 09:21:16 +0000

    Start on PresentProof Feature UI

---
+++ samples/blazorHosted/Source/Client/Features/PresentProof/Pages/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorHosted.Features.PresentProofs.Pages
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  public partial class Index
+  {
+    public const string RouteTemplate = "/proofs";
+
+    public static string GetRoute() => RouteTemplate;
+
+    protected override async Task OnInitializedAsync()
+    {
+      //_ = await Mediator.Send(new FetchProofsAction());
+
+      await base.OnInitializedAsync();
+    }
+  }
+}
---
+++ samples/blazorHosted/Source/Client/Features/PresentProof/PresentProofState.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorHosted.Features.PresentProof
+{
+  using BlazorState;
+  using Hyperledger.Aries.Features.PresentProof;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text.Json.Serialization;
+
+  public partial class PresentProofState : State<PresentProofState>
+  {
+    [JsonIgnore]
+    public IReadOnlyDictionary<string, ProofRecord> Proofs => _ProofRecords;
+
+    public IReadOnlyList<ProofRecord> ProofsAsList => _ProofRecords.Values.ToList();
+    private Dictionary<string, ProofRecord> _ProofRecords { get; set; }
+
+    public PresentProofState() { }
+
+    public override void Initialize()
+    {
+      _ProofRecords = new Dictionary<string, ProofRecord>();
+    }
+  }
+}
