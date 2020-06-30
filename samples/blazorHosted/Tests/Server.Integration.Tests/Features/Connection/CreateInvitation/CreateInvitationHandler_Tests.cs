f7e567b75d309e3414d199b78772f8346c6e2493
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-30 12:39:50 +0000

    CreateInvitationHandler Tests

--- samples/blazorHosted/Tests/Server.Integration.Tests/Features/Connection/CreateInvitation/CreateInvitationHandler_Tests.cs
+++ samples/blazorHosted/Tests/Server.Integration.Tests/Features/Connection/CreateInvitation/CreateInvitationHandler_Tests.cs
@@ -1,38 +1,54 @@
-﻿//namespace CreateInvitationHandler
-//{
-//  using System.Threading.Tasks;
-//  using System.Text.Json;
-//  using Microsoft.AspNetCore.Mvc.Testing;
-//  using BlazorHosted.Server.Integration.Tests.Infrastructure;
-//  using BlazorHosted.Features.Connections;
-//  using BlazorHosted.Server;
-//  using FluentAssertions;
+﻿namespace CreateInvitationHandler
+{
+  using System.Threading.Tasks;
+  using Microsoft.AspNetCore.Mvc.Testing;
+  using BlazorHosted.Server.Integration.Tests.Infrastructure;
+  using BlazorHosted.Features.Connections;
+  using BlazorHosted.Server;
+  using FluentAssertions;
+  using Newtonsoft.Json;
+  using Hyperledger.Aries.Features.DidExchange;
 
-//  public class Handle_Returns : BaseTest
-//  {
-//    private readonly CreateInvitationRequest CreateInvitationRequest;
+  public class Handle_Returns : BaseTest
+  {
+    private readonly CreateInvitationRequest CreateInvitationRequest;
 
-//    public Handle_Returns
-//    (
-//      WebApplicationFactory<Startup> aWebApplicationFactory,
-//      JsonSerializerOptions aJsonSerializerOptions
-//    ) : base(aWebApplicationFactory, aJsonSerializerOptions)
-//    {
-//      CreateInvitationRequest = new CreateInvitationRequest { Alias = "Alice" };
-//    }
+    public Handle_Returns
+    (
+      WebApplicationFactory<Startup> aWebApplicationFactory,
+      JsonSerializerSettings aJsonSerializerSettings
+    ) : base(aWebApplicationFactory, aJsonSerializerSettings)
+    {
+      // Set Valid values here
+      var inviteConfiguration = new InviteConfiguration
+      {
+        AutoAcceptConnection = true,
+      };
 
-//    public async Task CreateInvitationResponse()
-//    {
-//      CreateInvitationResponse CreateInvitationResponse = await Send(CreateInvitationRequest);
+      inviteConfiguration.MyAlias.Name = "Faber";
+      CreateInvitationRequest = new CreateInvitationRequest(inviteConfiguration);
+    }
 
-//      ValidateCreateInvitationResponse(CreateInvitationResponse);
-//    }
+    public async Task CreateInvitationResponse()
+    {
+      CreateInvitationResponse createInvitationResponse = await Send(CreateInvitationRequest);
 
-//    private void ValidateCreateInvitationResponse(CreateInvitationResponse aCreateInvitationResponse)
-//    {
-//      aCreateInvitationResponse.CorrelationId.Should().Be(CreateInvitationRequest.CorrelationId);
-//      // check Other properties here
-//    }
+      ValidateCreateInvitationResponse(createInvitationResponse);
+    }
 
-//  }
-//}
+    private void ValidateCreateInvitationResponse(CreateInvitationResponse aCreateInvitationResponse)
+    {
+      aCreateInvitationResponse.CorrelationId.Should().Be(CreateInvitationRequest.CorrelationId);
+      aCreateInvitationResponse.ConnectionInvitationMessage.Id.Should().NotBeNullOrEmpty();
+      aCreateInvitationResponse.ConnectionInvitationMessage.ImageUrl.Should().BeNull();
+      aCreateInvitationResponse.ConnectionInvitationMessage.Label.Should().Be("Faber");
+      aCreateInvitationResponse.ConnectionInvitationMessage.RecipientKeys.Count.Should().Be(1);
+      aCreateInvitationResponse.ConnectionInvitationMessage.RoutingKeys.Count.Should().Be(1);
+      aCreateInvitationResponse.ConnectionInvitationMessage.ServiceEndpoint.Should().Be("https://localhost:5551");
+      aCreateInvitationResponse.ConnectionInvitationMessage.Type.Should().Be("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/invitation");
+
+      aCreateInvitationResponse.InvitationUrl.Should().NotBeNull();
+    }
+
+  }
+}
