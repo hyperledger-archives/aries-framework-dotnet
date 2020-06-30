687e0dc587833ad172f69879b01434db23f339e1
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-30 15:20:00 +0000

    Add Test Helpers for each endpoint to setup and valid request and response

---
+++ samples/blazorHosted/Tests/Server.Integration.Tests/Features/Connection/CreateInvitation/CreateInvitationTestHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace TestHelpers
+{
+  using BlazorHosted.Features.Connections;
+  using FluentAssertions;
+  using Hyperledger.Aries.Features.DidExchange;
+
+  internal static class CreateInvitationTestHelper
+  {
+    internal static CreateInvitationRequest CreateValidCreateInvitationRequest()
+    {
+      var inviteConfiguration = new InviteConfiguration
+      {
+        AutoAcceptConnection = true,
+      };
+
+      inviteConfiguration.MyAlias.Name = "Faber";
+      return new CreateInvitationRequest(inviteConfiguration);
+    }
+
+    internal static void ValidateCreateInvitationResponse(CreateInvitationRequest aCreateInvitationRequest, CreateInvitationResponse aCreateInvitationResponse)
+    {
+      aCreateInvitationResponse.CorrelationId.Should().Be(aCreateInvitationRequest.CorrelationId);
+      aCreateInvitationResponse.ConnectionInvitationMessage.Id.Should().NotBeNullOrEmpty();
+      aCreateInvitationResponse.ConnectionInvitationMessage.ImageUrl.Should().BeNull();
+      aCreateInvitationResponse.ConnectionInvitationMessage.Label.Should().Be("Faber");
+      aCreateInvitationResponse.ConnectionInvitationMessage.RecipientKeys.Count.Should().Be(1);
+      aCreateInvitationResponse.ConnectionInvitationMessage.RoutingKeys.Count.Should().Be(1);
+      aCreateInvitationResponse.ConnectionInvitationMessage.ServiceEndpoint.Should().Be("https://localhost:5551");
+      aCreateInvitationResponse.ConnectionInvitationMessage.Type.Should().Be("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/invitation");
+
+      aCreateInvitationResponse.InvitationUrl.Should().NotBeNull();
+    }
+  }
+}
