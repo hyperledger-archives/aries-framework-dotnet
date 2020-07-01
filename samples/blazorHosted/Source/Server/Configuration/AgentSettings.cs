9735145a51e0b1bd6d73fbb110f57a331a22e7ec
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-07-01 16:01:50 +0000

    Add IssuerKeySeed to AgentSettings

--- samples/blazorHosted/Source/Server/Configuration/AgentSettings.cs
+++ samples/blazorHosted/Source/Server/Configuration/AgentSettings.cs
@@ -3,10 +3,11 @@
   public class AgentSettings
   {
     public string AgentName { get; set; }
-    public string IssuerKeySeed { get; set; }
     public string EndpointUri { get; set; }
     public string GenesisFilename { get; set; }
+    public string IssuerKeySeed { get; set; }
     public string PoolName { get; set; }
     public string WalletId { get; set; }
+    public string WalletKey { get; set; }
   }
 }
