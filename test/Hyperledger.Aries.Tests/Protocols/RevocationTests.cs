f4b187ff5a6bcf11f5e944e919e4c12de927e1d3
Tomislav Markovski <tmarkovski@gmail.com>
2020-07-13 11:43:30 -0400

    Additional test case for issuance after revocation

--- test/Hyperledger.Aries.Tests/Protocols/RevocationTests.cs
+++ test/Hyperledger.Aries.Tests/Protocols/RevocationTests.cs
@@ -263,6 +263,93 @@ namespace Hyperledger.Aries.Tests.Protocols
                 .VerifyProofAsync(pair.Agent1.Context, proofRecordIssuer.Id);
 
             Assert.False(valid);
+
+            // Issue another credential with revocation
+
+            (offer, record) = await pair.Agent1.Provider.GetRequiredService<ICredentialService>()
+                .CreateOfferAsync(pair.Agent1.Context, new OfferConfiguration
+                {
+                    CredentialDefinitionId = definitionWithRevocationId,
+                    IssuerDid = issuerConfiguration.IssuerDid,
+                    CredentialAttributeValues = new[]
+                    {
+                        new CredentialPreviewAttribute("name", "random"),
+                        new CredentialPreviewAttribute("age", "22")
+                    }
+                });
+            await pair.Agent1.Provider.GetRequiredService<IMessageService>()
+                .SendAsync(pair.Agent1.Context.Wallet, offer, pair.Connection1);
+
+            // Find credential for Agent 2 and accept all offers
+            credentials = await pair.Agent2.Provider.GetService<ICredentialService>()
+                .ListAsync(pair.Agent2.Context);
+            string latestCredentialId = null;
+            foreach (var credential in credentials.Where(x => x.State == CredentialState.Offered))
+            {
+                var (request, record1) = await pair.Agent2.Provider.GetService<ICredentialService>()
+                .CreateRequestAsync(pair.Agent2.Context, credential.Id);
+                await pair.Agent2.Provider.GetService<IMessageService>()
+                    .SendAsync(pair.Agent2.Context.Wallet, request, pair.Connection2);
+
+                latestCredentialId = record1.Id;
+                break;
+            }
+
+            // Issue credential
+            credentials = await pair.Agent1.Provider.GetService<ICredentialService>()
+                .ListRequestsAsync(pair.Agent1.Context);
+            foreach (var credential in credentials)
+            {
+                var (issue, _) = await pair.Agent1.Provider.GetRequiredService<ICredentialService>()
+                .CreateCredentialAsync(pair.Agent1.Context, credential.Id);
+                await pair.Agent1.Provider.GetService<IMessageService>()
+                    .SendAsync(pair.Agent1.Context.Wallet, issue, pair.Connection1);
+            }
+
+            // Verification - with revocation
+            now = (uint)DateTimeOffset.Now.ToUnixTimeSeconds();
+
+            (requestPresentationMessage, proofRecordIssuer) = await pair.Agent1.Provider.GetService<IProofService>()
+                .CreateRequestAsync(pair.Agent1.Context, new ProofRequest
+                {
+                    Name = "Test Verification",
+                    Version = "1.0",
+                    Nonce = await AnonCreds.GenerateNonceAsync(),
+                    RequestedAttributes = new Dictionary<string, ProofAttributeInfo>
+                    {
+                        { "id-verification", new ProofAttributeInfo { Names = new [] { "name", "age" } } }
+                    },
+                    NonRevoked = new RevocationInterval
+                    {
+                        From = 0,
+                        To = now
+                    }
+                });
+
+            proofRecordHolder = await pair.Agent2.Provider.GetService<IProofService>()
+                .ProcessRequestAsync(pair.Agent2.Context, requestPresentationMessage, pair.Connection2);
+            
+            (presentationMessage, _) = await pair.Agent2.Provider.GetService<IProofService>()
+                .CreatePresentationAsync(pair.Agent2.Context, proofRecordHolder.Id, new RequestedCredentials
+                {
+                    RequestedAttributes = new Dictionary<string, RequestedAttribute>
+                    {
+                        { "id-verification", new RequestedAttribute
+                            {
+                                CredentialId = latestCredentialId,
+                                Revealed = true
+                            }
+                        }
+                    }
+                });
+
+            proofRecordIssuer = await pair.Agent1.Provider.GetService<IProofService>()
+                .ProcessPresentationAsync(pair.Agent1.Context, presentationMessage);
+
+            valid = await pair.Agent1.Provider.GetService<IProofService>()
+                .VerifyProofAsync(pair.Agent1.Context, proofRecordIssuer.Id);
+
+            Assert.True(valid);
         }
 
         //[Fact]
