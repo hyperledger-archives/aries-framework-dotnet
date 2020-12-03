f2debd1b76f4ace3e9123f22e493cefaffaef8ea
CHempel-esatus <c.hempel@esatus.com>
2020-12-03 17:14:43 +0100

    Added missing property UseMessageTypesHttps to DefaultAgentContext and IAgentContext
    Signed-off-by: CHempel-esatus <c.hempel@esatus.com>

--- src/Hyperledger.Aries/Agents/DefaultAgentContext.cs
+++ src/Hyperledger.Aries/Agents/DefaultAgentContext.cs
@@ -36,6 +36,12 @@ namespace Hyperledger.Aries.Agents
         public IList<MessageType> SupportedMessages { get; set; }
 
         /// <summary>
+        /// Gets or sets the value for UseMessageTypesHttps.
+        /// </summary>
+        /// <value>True if to use UseMessageTypesHttps.</value>
+        public bool UseMessageTypesHttps { get; set; }
+
+        /// <summary>
         /// Gets or sets the configured agent for this context
         /// </summary>
         public IAgent Agent { get; set; }
