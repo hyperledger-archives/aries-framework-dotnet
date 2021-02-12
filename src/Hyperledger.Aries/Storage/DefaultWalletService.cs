aca967568d4ada09c7a1484131b566888bb11caf
Andraz Cuderman <cuderman.andraz@gmail.com>
2021-02-12 14:00:55 +0100

    Resolve race condition when opening a new wallet (#169)
    * Add semaphore around opening and caching new wallet
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Add support for using PostgreSQL database for storing mediator agent inbox records (#166)
    
    * Add support for using PostgreSQL database for mediator agent inbox records
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Add all storage configuration options with default values
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Update Wallet storage configuration defaults
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Update Wallet storage configuration test description
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Update default unit test expectation
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Delegate setting default values to the postgres plugin
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Replace catch with cache get inside semaphore
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>
    
    * Open wallet with mutex
    
    Signed-off-by: Andraž Cuderman <cuderman.andraz@gmail.com>

--- src/Hyperledger.Aries/Storage/DefaultWalletService.cs
+++ src/Hyperledger.Aries/Storage/DefaultWalletService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Threading;
 using System.Threading.Tasks;
 using Hyperledger.Aries.Extensions;
 using Hyperledger.Indy.WalletApi;
@@ -14,22 +15,42 @@ namespace Hyperledger.Aries.Storage
         protected static readonly ConcurrentDictionary<string, Wallet> Wallets =
             new ConcurrentDictionary<string, Wallet>();
 
+        /// <summary>
+        /// Mutex semaphore for opening a new (not cached) wallet
+        /// </summary>
+        private static readonly SemaphoreSlim OpenWalletSemaphore = new SemaphoreSlim(1,1);
+
         /// <inheritdoc />
         public virtual async Task<Wallet> GetWalletAsync(WalletConfiguration configuration, WalletCredentials credentials)
         {
             var wallet = GetWalletFromCache(configuration);
 
-            if (wallet != null)
-                return wallet;
+            if (wallet == null)
+            {
+                wallet = await OpenWalletWithMutexAsync(configuration, credentials);
+            }
 
+            return wallet;
+        }
+
+        private async Task<Wallet> OpenWalletWithMutexAsync(WalletConfiguration configuration, WalletCredentials credentials)
+        {
+            Wallet wallet;
+
+            await OpenWalletSemaphore.WaitAsync();
             try
             {
-                wallet = await Wallet.OpenWalletAsync(configuration.ToJson(), credentials.ToJson());
-                Wallets.TryAdd(configuration.Id, wallet);
-            }
-            catch (WalletAlreadyOpenedException)
-            {
                 wallet = GetWalletFromCache(configuration);
+
+                if (wallet == null)
+                {
+                    wallet = await Wallet.OpenWalletAsync(configuration.ToJson(), credentials.ToJson());
+                    Wallets.TryAdd(configuration.Id, wallet);
+                }
+            }
+            finally
+            {
+                OpenWalletSemaphore.Release();
             }
 
             return wallet;
